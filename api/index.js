56a070cb72bfc6e7460185a42cede45172e9b149
okellogeorgeokoth <okoth603@gmail.com>
2024-10-15 15:20:24 +0300

    Creating API route

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -11,3 +11,13 @@ mongoose
 .catch((err) => {
     console.log(err);
 });
+
+const app =express();
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+});
+
+app.get( '/test', (req , res)=> {
+    res.send('Hello world!');
+    }
+);
