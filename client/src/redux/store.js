d8ea4404212b37130f4ccd1fb6210c90fe059385
okellogeorgeokoth <okoth603@gmail.com>
2024-11-17 15:29:29 +0300

    Updated Middleware for Store redux section

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 
+
 const store = configureStore({
   reducer: {
-    user: userReducer,
-  },
+    user: userReducer },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false,}),
+
 });
 
 export default store;
